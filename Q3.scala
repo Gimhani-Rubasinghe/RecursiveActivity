00d8c40a4df6e9bf3fe972c7fba9fbae211c7ef0
Gimhani-Rubasinghe <62197172+Gimhani-Rubasinghe@users.noreply.github.com>
2021-07-18 18:29:19 +0530

    Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -3,9 +3,9 @@ object Q3 {
   def main(Args:Array[String]){
     Sum(5)//display 15(1+2+3+4+5)
   }
-  def Sum(n:Int,i:Int=1,result:Int=0):Any ={//function to return the addition of numbers from1 to n
+  def Sum(n:Int,i:Int=1,result:Int=0):Any ={//function to return the addition of numbers from 1 to n
     if(n<1){
-      println("Please enter positive integer")
+      println("n should be greater than 0")
     }
     else if(i<=n){
       return Sum(n,i+1,result+i)
